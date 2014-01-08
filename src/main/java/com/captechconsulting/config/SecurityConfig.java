c32bd9e163cec1abe3956dbaf0fdc6e1d7235802
Jens Alm <jens@ulmus.org>
2014-01-08 18:14:14 -0500

    Added a authentication entry point.

--- src/main/java/com/captechconsulting/config/SecurityConfig.java
+++ src/main/java/com/captechconsulting/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 import javax.servlet.ServletException;
@@ -23,6 +25,7 @@ import java.io.PrintWriter;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String ACCESS_DENIED_JSON = "{\"message\":\"You are not privileged to request this resource.\", \"access-denied\":true,\"cause\":\"AUTHORIZATION_FAILURE\"}";
+    private static final String UNAUTHORIZED_JSON = "{\"message\":\"Full authentication is required to access this resource.\", \"access-denied\":true,\"cause\":\"NOT AUTHENTICATED\"}";
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -48,7 +51,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 and().
 
-                exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler()).
+                exceptionHandling().
+                accessDeniedHandler(new CustomAccessDeniedHandler()).
+                authenticationEntryPoint(new CustomAuthenticationEntryPoint()).
 
                 and().
 
@@ -65,12 +70,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
 
             response.setContentType(Versions.V1_0);
-            response.sendError(HttpServletResponse.SC_FORBIDDEN, accessDeniedException.getMessage());
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
             PrintWriter out = response.getWriter();
             out.print(ACCESS_DENIED_JSON);
             out.flush();
             out.close();
 
+        }
+    }
+
+    public static class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+        @Override
+        public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+            response.setContentType(Versions.V1_0);
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            PrintWriter out = response.getWriter();
+            out.print(UNAUTHORIZED_JSON);
+            out.flush();
+            out.close();
         }
     }
 
