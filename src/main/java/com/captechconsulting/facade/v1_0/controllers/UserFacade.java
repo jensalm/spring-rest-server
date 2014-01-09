fddeacf9e5be4e1b46a03fde7c043ec8a05b6f3e
Jens Alm <jens@ulmus.org>
2014-01-08 21:07:41 -0500

    Changed to Controller which implicitly adds responsebody to all methods.

--- src/main/java/com/captechconsulting/facade/v1_0/controllers/UserFacade.java
+++ src/main/java/com/captechconsulting/facade/v1_0/controllers/UserFacade.java
@@ -7,14 +7,13 @@ import com.captechconsulting.facade.v1_0.data.UserVO;
 import org.dozer.DozerBeanMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @Transactional
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserFacade {
 
@@ -25,13 +24,13 @@ public class UserFacade {
     private DozerBeanMapper mapper;
 
     @RequestMapping(value = "/{userId}", method = RequestMethod.GET, produces = Versions.V1_0, consumes = Versions.V1_0)
-    public @ResponseBody UserVO getUser(@PathVariable long userId) {
+    public UserVO getUser(@PathVariable long userId) {
         User user = userService.getUser(userId);
         return mapper.map(user, UserVO.class);
     }
 
     @RequestMapping(value = "/{userId}", method = RequestMethod.PUT, produces = Versions.V1_0, consumes = Versions.V1_0)
-    public @ResponseBody UserVO update(@PathVariable long userId, @Valid @RequestBody UserVO user) {
+    public UserVO update(@PathVariable long userId, @Valid @RequestBody UserVO user) {
         User previouslyPersisted = userService.getUser(userId);
         mapper.map(user, previouslyPersisted);
         User persisted = userService.store(previouslyPersisted);
@@ -40,7 +39,7 @@ public class UserFacade {
 
     @RequestMapping(method = RequestMethod.POST, produces = Versions.V1_0, consumes = Versions.V1_0)
     @ResponseStatus(HttpStatus.CREATED)
-    public @ResponseBody UserVO create(@Valid @RequestBody UserVO user) {
+    public UserVO create(@Valid @RequestBody UserVO user) {
         User mappedUser = mapper.map(user, User.class);
         User persisted = userService.store(mappedUser);
         return mapper.map(persisted, UserVO.class);
@@ -48,7 +47,7 @@ public class UserFacade {
 
     @RequestMapping(value = "/{userId}", method = RequestMethod.DELETE, produces = Versions.V1_0, consumes = Versions.V1_0)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public @ResponseBody UserVO remove(@PathVariable long userId) {
+    public UserVO remove(@PathVariable long userId) {
         User user = userService.getUser(userId);
         userService.delete(user);
         return null;
